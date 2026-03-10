af516b16c3df97a06d39abfd2930ba0618ebcf64
Alexander Gulidov <derideveloper@gmail.com>
2026-03-10 20:32:58 +0300

    upd

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,5 @@
 export const API_URL = import.meta.env.VITE_API_URL;
 export const ENVIRONMENT = import.meta.env.VITE_ENVIRONMENT;
+
+console.log("VITE_API_URL:", import.meta.env.VITE_API_URL);
+console.log("All env vars:", import.meta.env);
