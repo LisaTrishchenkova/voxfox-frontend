322e181e44e179ebc462f3ac3ff5e876368b446f
Lisa <ecernenko090@gmail.com>
2026-04-30 16:15:48 +0300

    chore: disable commitlint line length checks (#167)

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -4,4 +4,9 @@ export default {
     (message) => /^Merge/.test(message),
     (message) => /\(#\d+\)$/.test(message),
   ],
+  rules: {
+    "header-max-length": [0, "always", 100],
+    "body-max-line-length": [0, "always", 100],
+    "footer-max-line-length": [0, "always", 100],
+  },
 };
