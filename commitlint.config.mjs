d39a8e8e2c17e90a2a8fdbc1566b7bb382f22d28
Alexander Gulidov <derideveloper@gmail.com>
2026-05-30 23:06:34 +0300

    ci: fix commitlint config

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,12 +1,33 @@
 export default {
   extends: ["@commitlint/config-conventional"],
+  rules: {
+    "subject-case": [0],
+    "header-max-length": [2, "always", 200],
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat",
+        "fix",
+        "docs",
+        "style",
+        "refactor",
+        "perf",
+        "test",
+        "chore",
+        "ci",
+        "build",
+        "revert",
+      ],
+    ],
+  },
   ignores: [
     (message) => /^Merge/.test(message),
-    (message) => /\(#\d+\)$/.test(message),
+    (message) => /^Merge pull request/.test(message),
+    (message) => /^Dev \(#\d+\)/.test(message),
+    (message) => /^Test \(#\d+\)/.test(message),
+    (message) => /^Staging \(#\d+\)/.test(message),
+    (message) => /^Update \.gitignore/.test(message),
+    (message) => message.includes("squash"),
   ],
-  rules: {
-    "header-max-length": [0, "always", 100],
-    "body-max-line-length": [0, "always", 100],
-    "footer-max-line-length": [0, "always", 100],
-  },
 };
